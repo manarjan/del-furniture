850ceaabf831a3c943a05f3aaf8bd775ed967094
Manarjan Singh Ghai <manarjan.s.ghai@gmail.com>
2020-09-10 22:37:32 +0530

    Update port, take from env variable

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import {
   NestFastifyApplication,
-  FastifyAdapter,
+  FastifyAdapter
 } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 import { join } from 'path';
 
 async function bootstrap() {
+  const port: number = +process.env.PORT || 4000;
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
-    new FastifyAdapter(),
+    new FastifyAdapter()
   );
   app.useStaticAssets({
     root: join(__dirname, '..', 'public'),
-    prefix: '/public/',
+    prefix: '/public/'
   });
   app.setViewEngine({
     engine: {
-      handlebars: require('handlebars'),
+      handlebars: require('handlebars')
     },
-    templates: join(__dirname, '..', 'views'),
+    templates: join(__dirname, '..', 'views')
   });
-  await app.listen(4000);
+  await app.listen(port);
 }
 bootstrap();
